dcbdafd45f8641e6a83dc95af409ce0d7d25081c
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-10 17:22:12 +0530

    add side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
@@ -23,5 +23,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogout(): void {
+    //
   }
 }
