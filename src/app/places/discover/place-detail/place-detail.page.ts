2b2407c9ecfa728298b95f6d6cddb51372171a43
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-10 11:38:23 +0530

    navigating into detail and back

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private _router: Router, private _navController: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBookPlace(): void {
+    // this._router.navigateByUrl('/places/tabs/discover');
+    // Use Nav Controller, which under the hood uses angular router
+    // but displays a backward animation than default forward
+    this._navController.navigateBack('/places/tabs/discover');
   }
-
 }
