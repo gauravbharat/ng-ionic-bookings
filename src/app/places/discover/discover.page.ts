4f05507c48dcafcb710296e385dfdf192bb8fb03
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-10 11:08:00 +0530

    outputting places on discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-discover',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
 
-  constructor() { }
+  constructor(private _placesService: PlacesService) {}
 
   ngOnInit() {
+    this.loadedPlaces = this._placesService.places;
+    console.log('this.loadedPlaces', this.loadedPlaces);
   }
 
+  // onViewWillEnter() {
+  //   this.loadedPlaces = this._placesService.places;
+  //   console.log('this.loadedPlaces', this.loadedPlaces);
+  // }
 }
