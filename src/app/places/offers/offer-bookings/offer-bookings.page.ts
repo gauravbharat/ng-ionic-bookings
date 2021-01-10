7290f4bea8208773ebd89ed96a9189bb92cf1bf5
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-10 12:35:16 +0530

    edit routing from offer bookings

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -7,9 +10,24 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./offer-bookings.page.scss'],
 })
 export class OfferBookingsPage implements OnInit {
-  constructor(private _navController: NavController) {}
+  place: Place;
 
-  ngOnInit() {}
+  constructor(
+    private _navController: NavController,
+    private _activatedRoute: ActivatedRoute,
+    private placesService: PlacesService
+  ) {}
+
+  ngOnInit() {
+    this._activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this._navController.navigateBack('/places/tabs/offers');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
+  }
 
   onBookOffer(): void {
     this._navController.navigateBack('/places/tabs/offers');
