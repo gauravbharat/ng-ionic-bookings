cb3fe7f9e0cd7620fe6ab0d5bdb1faf35d4562e2
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-15 08:28:28 +0530

    added sliding item to offers

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding, NavController } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -9,10 +10,25 @@ import { PlacesService } from '../places.service';
 })
 export class OffersPage implements OnInit {
   offers: Place[];
-  constructor(private _placesService: PlacesService) {}
+  constructor(
+    private _placesService: PlacesService,
+    private _navController: NavController
+  ) {}
 
   ngOnInit() {
     this.offers = this._placesService.places;
     console.log('this.offers', this.offers);
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding): void {
+    slidingItem.close();
+    this._navController.navigateForward([
+      '/',
+      'places',
+      'tabs',
+      'offers',
+      'edit',
+      offerId,
+    ]);
   }
 }
