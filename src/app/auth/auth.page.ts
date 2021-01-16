c17a50b030aa3a3203e2fb33bd588654365298e6
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-16 12:22:16 +0530

    added template form for login

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -32,5 +33,9 @@ export class AuthPage implements OnInit {
           this.isLoading = false;
         }, 2000);
       });
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form);
   }
 }
