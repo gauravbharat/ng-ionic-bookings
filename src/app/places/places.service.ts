41735caa38955664d34abe0b72254135873e7abb
Gaurav Mendse <gaurav.mendse@icloud.com>
2021-01-17 17:12:45 +0530

    send data to firebase server

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 
 import { AuthService } from '../auth/auth.service';
 import { Place } from './place.model';
@@ -42,7 +43,7 @@ export class PlacesService {
     ),
   ]);
 
-  constructor(private _authService: AuthService) {}
+  constructor(private _authService: AuthService, private _http: HttpClient) {}
 
   get places() {
     return this._places.asObservable();
@@ -64,6 +65,8 @@ export class PlacesService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
+
     const newPlace = new Place(
       Math.random().toString(),
       title,
@@ -75,11 +78,25 @@ export class PlacesService {
       this._authService.userId
     );
 
-    return this.places.pipe(
-      take(1),
-      delay(1000),
-      tap((places) => this._places.next(places.concat(newPlace)))
-    );
+    return this._http
+      .post<{ name: string }>(
+        `https://ing-bookings-default-rtdb.firebaseio.com/offered-places.json`,
+        {
+          ...newPlace,
+          id: null,
+        }
+      )
+      .pipe(
+        switchMap((resData) => {
+          generatedId = resData.name;
+          return this.places;
+        }),
+        take(1),
+        tap((places) => {
+          newPlace.id = generatedId;
+          this._places.next(places.concat(newPlace));
+        })
+      );
   }
 
   updatePlace(placeId: string, title: string, description: string) {
